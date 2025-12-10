a1d2edaa9f0600b8c49052022d1af2d2338e90a5
MAULIK PATEL <maulikspatel295@gmail.com>
2025-12-10 13:24:29 +0530

    Added ResponseEntity concept with basic HTTP status codes

--- src/main/java/com/leotechindia/quiz_app/controller/QuestionController.java
+++ src/main/java/com/leotechindia/quiz_app/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.leotechindia.quiz_app.controller;
 import com.leotechindia.quiz_app.model.Question;
 import com.leotechindia.quiz_app.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,25 +17,25 @@ public class QuestionController {
 
     // Search All available questions
     @GetMapping("all")
-    public List<Question> getAllQuestions() {
+    public ResponseEntity<List<Question>> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 
     // Search question with specified ID
     @GetMapping("id/{search_id}")
-    public Question getQuestionById(@PathVariable("search_id") int search_id) {
+    public ResponseEntity<Question> getQuestionById(@PathVariable("search_id") int search_id) {
         return questionService.getQuestionById(search_id);
     }
 
     // Search question with specified CATEGORY
     @GetMapping("cat/{search_category}")
-    public List<Question> getQuestionsByCategory(@PathVariable("search_category") String category) {
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable("search_category") String category) {
         return questionService.getQuestionsByCategory(category);
     }
 
     // Add question
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question newQuestion) {
+    public ResponseEntity<String> addQuestion(@RequestBody Question newQuestion) {
         return questionService.addQuestion(newQuestion);
     }
 }
--- src/main/java/com/leotechindia/quiz_app/service/QuestionService.java
+++ src/main/java/com/leotechindia/quiz_app/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.leotechindia.quiz_app.service;
 import com.leotechindia.quiz_app.dao.QuestionDao;
 import com.leotechindia.quiz_app.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,21 +17,41 @@ public class QuestionService {
     @Autowired
     private QuestionDao questionDao;
 
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public Question getQuestionById(int searchId) {
-        Optional<Question> optQuestion = questionDao.findById(searchId);
-        return optQuestion.orElse(null);
+    public ResponseEntity<Question> getQuestionById(int searchId) {
+        try {
+            Optional<Question> optQuestion = questionDao.findById(searchId);
+            return new ResponseEntity<>(optQuestion.orElse(null), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new Question(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public String addQuestion(Question newQuestion) {
-        questionDao.save(newQuestion);
-        return "Added new Question in " + newQuestion.getCategory() + " category !";
+    public ResponseEntity<String> addQuestion(Question newQuestion) {
+        try {
+            questionDao.save(newQuestion);
+            return new ResponseEntity<>("Added new Question in " + newQuestion.getCategory() + " category !", HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST.getReasonPhrase(), HttpStatus.BAD_REQUEST);
     }
 }
